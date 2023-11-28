409ab914b6010ea959aa7d3c4165cde78efb7a17
Guillermo Garcia <bitcoder@gmail.com>
2023-11-28 03:07:00 -0500

    Adding requests library to start developing. Added a dummy code to test initial execution

--- elmito_cli/__init__.py
+++ elmito_cli/__init__.py
@@ -1,1 +1,13 @@
 __version__ = '0.1.0'
+
+import click
+
+@click.command()
+@click.option("--path", prompt="Enter path to analize",
+              help="Folder path to look for audio files.")
+def getGenre(path):
+    """CLI to prepare and manage music selection."""
+    click.echo("Path to look for files is: %s" % path)
+
+if __name__ == '__main__':
+    getGenre()
